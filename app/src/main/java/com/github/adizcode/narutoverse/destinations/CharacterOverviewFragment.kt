f1d2c29888705833c9d0c149bf7b286100b0df0c
Advitiay Anand <adizcode@pop-os.localdomain>
2021-11-08 22:20:01 +0530

    Add app name in manifest and add logs

--- app/src/main/java/com/github/adizcode/narutoverse/destinations/CharacterOverviewFragment.kt
+++ app/src/main/java/com/github/adizcode/narutoverse/destinations/CharacterOverviewFragment.kt
@@ -1,5 +1,6 @@
 package com.github.adizcode.narutoverse.destinations
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,14 +10,28 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.github.adizcode.narutoverse.R
 import com.github.adizcode.narutoverse.databinding.FragmentCharacterOverviewBinding
+import timber.log.Timber
 
 class CharacterOverviewFragment : Fragment() {
     private lateinit var binding: FragmentCharacterOverviewBinding
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Timber.i("onAttach")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Timber.i("onCreate")
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+        Timber.i("onCreateView")
+
         // Inflate the layout for this fragment
         super.onCreateView(inflater, container, savedInstanceState)
         binding = DataBindingUtil.inflate(
@@ -42,6 +57,46 @@ class CharacterOverviewFragment : Fragment() {
         binding.readMoreButton.setOnClickListener(clickListener)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Timber.i("onViewCreated")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Timber.i("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.i("onStop")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Timber.i("onDestroyView")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("onDestroy")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Timber.i("onDetach")
     }
 
 }
