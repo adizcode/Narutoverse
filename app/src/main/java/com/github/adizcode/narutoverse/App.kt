69801537f35269fcb687e413942cb87e0792f0e1
Advitiay Anand <adizcode@pop-os.localdomain>
2021-11-08 22:02:00 +0530

    Add Timber

---
+++ app/src/main/java/com/github/adizcode/narutoverse/App.kt
@@ -0,0 +1,11 @@
+package com.github.adizcode.narutoverse
+
+import android.app.Application
+import timber.log.Timber
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
